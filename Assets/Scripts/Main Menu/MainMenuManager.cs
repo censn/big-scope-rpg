486fbf7fa415f380068a7c3b169b00bfa490f9db
censn <hoveranyonehonesty@gmail.com>
2018-04-30 14:52:54 -0400

    -add plugins -basic scene setup (+1 squashed commits)
    Squashed commits:
    
    [25e4869] it begins

---
+++ Assets/Scripts/Main Menu/MainMenuManager.cs
@@ -0,0 +1,34 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour {
+
+    [SerializeField]
+    private TextMeshProUGUI connecting;
+
+    [SerializeField]
+    private Button joinLobby;
+
+    [SerializeField]
+    private PopulationTracker population;
+
+    private bool CanJoinLobby {
+        get {
+            return population.IsBelowCapacity;
+        }
+    }
+
+    public void JoinLobby() {
+        connecting.enabled = true;
+        PhotonNetwork.ConnectUsingSettings("apple");
+        PhotonNetwork.JoinLobby();
+        SceneManager.LoadScene(1);
+    }
+
+    private void Update() {
+        int playerCount = PhotonNetwork.countOfPlayers;
+        joinLobby.enabled = CanJoinLobby;
+    }
+}
---
+++ Assets/Scripts/Utility/PopulationTracker.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class PopulationTracker : MonoBehaviour {
+    private const int MAX_ALLOWED_PLAYERS = 20;
+
+    [SerializeField]
+    private TextMeshProUGUI text;
+
+    public bool IsBelowCapacity {
+        get {
+            return PhotonNetwork.countOfPlayers < MAX_ALLOWED_PLAYERS;
+        }
+    }
+
+    private void Update() {
+        text.SetText("{0}/{1}", PhotonNetwork.countOfPlayers, MAX_ALLOWED_PLAYERS);
+    }
+}
